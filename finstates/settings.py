43c5e3bc72c4a96bc57eb7ea0a15fe80cdc2d19d
eddie1979 <ed.emdless-trip.boc26@ezweb.ne.jp>
2020-04-18 12:09:10 +0900

    settings.py-changes
    ・INSTALLED_APPSに'finchart'を記述
    
    ・TEMPLATESに'DIRS': [os.path.join(BASE_DIR, 'templates')]を記述
    
    ・LANGUAGE_CODEに'ja'を記述
    
    ・TIME_ZONEに'Asia/Tokyo'を記述
    
    ・STATIC_ROOT = os.path.join(BASE_DIR, 'static')を追加

--- finstates/settings.py
+++ finstates/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'finchart'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'finstates.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
@@ -117,4 +118,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
