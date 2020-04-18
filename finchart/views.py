b4797c448c9506b515c0beb9899f9f8acb4744b7
eddie1979 <ed.emdless-trip.boc26@ezweb.ne.jp>
2020-04-18 13:21:56 +0900

    create-view
    index.htmlをインポートしてビューを作製。

--- finchart/views.py
+++ finchart/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import generic
+from django.veiws.generic import TemplateView
+
+class IndexTemplateView(TemplateView):
+    template_name = 'finchart/index'
