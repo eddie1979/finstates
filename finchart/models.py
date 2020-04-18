489e52dfd147f5264bb86d94f7b0b54c8dbdccf3
eddie1979 <ed.emdless-trip.boc26@ezweb.ne.jp>
2020-04-18 12:17:23 +0900

    models.py-add-company
    models.pyにcompanyモデルを記述。

--- finchart/models.py
+++ finchart/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Company(models.Model):
+  name = models.CharField("会社名", max_lenght = 30, blank = false)
+
+
+    def __str__(self):
+      return str(self.name)
+
+#----ここまで----
