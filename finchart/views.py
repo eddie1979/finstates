0b9f510436fb592ef213a6512999dbbd095039f1
eddie1979 <ed.emdless-trip.boc26@ezweb.ne.jp>
2020-04-18 21:02:49 +0900

    add-datatables
    ・jQueryのライブラリである、DataTablesを追加します。

--- finchart/views.py
+++ finchart/views.py
@@ -3,6 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 from django.views import generic
 from django.views.generic import TemplateView
+from .models import Fstatement
 
-class IndexTemplateView(TemplateView):
+class IndexView(TemplateView):
     template_name = 'finchart/index.html'
+
+
+    def get_context_data(self, **kwargs):
+        fstatement_list = Fstatement.objects.all().order_by('company')
+        params = {
+            'fstatement_list': fstatement_list,
+        }
+        return params
