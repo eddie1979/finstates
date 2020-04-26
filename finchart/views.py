bc81da3fa1908809d6d989352ab8935224ddfcd6
eddie1979 <ed.emdless-trip.boc26@ezweb.ne.jp>
2020-04-26 20:08:26 +0900

    add-CompanymView(DetailView)
    classにCompanyView(DetailView)を追記してmodel = CompanyによってComapnyモデルの詳細画面を取り扱えるように設定しました。

--- finchart/views.py
+++ finchart/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.views import generic
-from django.views.generic import TemplateView
-from .models import Fstatement
+from django.views.generic import TemplateView, DetailView
+from .models import Company, Fstatement
 
 class IndexView(TemplateView):
     template_name = 'finchart/index.html'
@@ -12,6 +10,18 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         fstatement_list = Fstatement.objects.all().order_by('company')
         params = {
+            'fstatement_list': fstatement_list,
+        }
+        return params
+        
+class CompanyView(DetailView):
+    model = Company
+
+    def get_context_data(self, **kwargs):
+        company_name = kwargs['object'].name
+        fstatement_list = Fstatement.objects.filter(company=kwargs['object']).order_by('-fiscal_year')[:4]
+        params = {
+            'company_name': company_name,
             'fstatement_list': fstatement_list,
         }
         return params
